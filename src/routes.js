62419139b37fe3e709d836e97ff8bc36455c20ed
Held Grijo <von.grijo@pmm.am.gov.br>
2019-07-18 17:26:40 -0400

    Nunjucks, Templates, serving public files

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.send('Hello World X')
-})
+routes.get('/', (req, res) => res.render('auth/signup'))
 module.exports = routes
